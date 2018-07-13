8c53533619746894180d4c04e291346f5dcd0985
unknown <PRESTEC@Poncas05.fib.upc.edu>
2018-07-13 19:39:15 +0200

    Put latest photo the first in display

--- main3.py
+++ main3.py
@@ -6,13 +6,16 @@ import tkinter
 
 from itertools import cycle, chain
 from PIL import Image, ImageTk
+from Bot import Bot
 
 path = "./img"
+bot_image_path = 'img/'
+bot_token = '471926478:AAECF2kaSTezJH7_fhIYAzGKKw3cNXQLZ7g'
 
 
 class Slideshow(tkinter.Tk):
     """Display a slideshow from a list of filenames"""
-    def __init__(self, images, slide_interval):
+    def __init__(self, slide_interval, bot):
         """Initialize
         
         images = a list of filename 
@@ -21,19 +24,21 @@ class Slideshow(tkinter.Tk):
         tkinter.Tk.__init__(self)
         self.geometry("+0+0")
         self.slide_interval = slide_interval
-        self.images = None
-        self.set_images(images)
+        self.images = []
+        self.get_images_from_disk()
+        self.set_images(self.images)
         self.slide = tkinter.Label(self)
         self.slide.pack()
-        self.next_image_index = 0
+        self.next_image_index = len(self.images) - 1
+        self.bot = bot
 
     def set_images(self, images):
          self.images = images
     
     def update_index(self):
-        self.next_image_index += 1
-        if self.next_image_index == len(self.images):
-            self.next_image_index = 0
+        self.next_image_index -= 1
+        if self.next_image_index < 0:
+            self.next_image_index = len(self.images) - 1
 
     def center(self):
         """Center the slide window on the screen"""
@@ -45,34 +50,54 @@ class Slideshow(tkinter.Tk):
         y = h / 2 - size[1] / 2
         self.geometry("+%d+%d" % (x, y))
 
+    def resize_image(self, image):
+        size = image.size
+        w_dif = self.winfo_screenwidth() - size[0] 
+        h_dif = self.winfo_screenheight() - size[1]
+        w_ratio = 1
+        h_ratio = 1
+        if w_dif < 0:
+            w_ratio = size[0]/self.winfo_screenwidth()
+        if h_dif < 0:
+            h_ratio = size[1]/self.winfo_screenheight()
+        ratio = max(w_ratio, h_ratio)
+        image = image.resize((int(size[0]/ratio), int(size[1]/ratio)), Image.ANTIALIAS)
+        return image
+
     def set_image(self):
         """Setup image to be displayed"""
         self.image_name = self.images[self.next_image_index]
         self.update_index()
         filename, ext = os.path.splitext(self.image_name)
-        self.image = ImageTk.PhotoImage(Image.open(path + '/' + self.image_name))
+        image = Image.open(path + '/' + self.image_name)
+        image = self.resize_image(image)
+        self.image = ImageTk.PhotoImage(image)
         
     def main(self):
         """Display the images"""
-        new_images = get_images_from_disk()
-        self.set_images(new_images)
+        self.get_images_from_disk()
+        self.set_images(self.images)
         self.set_image()
         self.slide.config(image=self.image)
         self.title(self.image_name)
-        self.center()
+        #self.center()
         self.after(self.slide_interval, self.start)
     
     def start(self):
         """Start method"""
         self.main()
+        bot.updatePictures()
         self.mainloop()
 
-def get_images_from_disk():
-    import glob
-    images = glob.glob("*.jpg")
-    exts = ["jpg", "bmp", "png", "gif", "jpeg"]
-    images = [fn for fn in os.listdir(path) if any(fn.endswith(ext) for ext in exts)]
-    return images
+    def get_images_from_disk(self):
+        import glob
+        images = glob.glob("*.jpg")
+        exts = ["jpg", "bmp", "png", "gif", "jpeg"]
+        images = [fn for fn in os.listdir(path) if any(fn.endswith(ext) for ext in exts)]
+        images.sort()
+        if len(images) > len(self.images):
+            self.images = images
+            self.next_image_index = len(self.images) - 1
 
 if __name__ == "__main__":
     slide_interval = 2500
@@ -88,8 +113,9 @@ if __name__ == "__main__":
     # "." us the directory the script is in.
     # exts is the file extentions to use.  it can be any extention that pillow supports
     # http://pillow.readthedocs.io/en/3.3.x/handbook/image-file-formats.html
-    images = get_images_from_disk()
+    bot = Bot(bot_token, bot_image_path)
+    bot.updatePictures()
 
     # start the slideshow
-    slideshow = Slideshow(images, slide_interval)
+    slideshow = Slideshow(slide_interval, bot)
     slideshow.start()
